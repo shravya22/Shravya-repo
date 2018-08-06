75f8ab3c83f46fb4f320532f79545c46be4e751b
sgunnala <sgunnala@DIN73002727.corp.capgemini.com>
2018-08-06 10:30:51 +0530

    my first commit

---
+++ Day1GitDemo/src/main/java/com/cap/MainTestClass.java
@@ -0,0 +1,16 @@
+package com.cap;
+
+public class MainTestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello World");
+greet();
+	}
+
+	public static void greet() {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+	}
+
+}
