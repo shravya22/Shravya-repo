9f5f82919eed0d3ce3b1f20ba42a67863a8f7619
gchintam <gchintam@DIN73002599.corp.capgemini.com>
2018-08-06 14:41:20 +0530

    no changes

--- Day1GitDemo/src/main/java/com/cap/SalesClass.java
+++ Day1GitDemo/src/main/java/com/cap/SalesClass.java
@@ -2,13 +2,16 @@ package com.cap;
 
 public class SalesClass {
 	int[] arr= {1,2,3,40,50};
-
+	int sum=0;
 	public void show() {
+	
 		// TODO Auto-generated method stub
 		for(int num:arr)
 		{
+			
 			System.out.println(num);
 		}
+
 	}
 	
 }
