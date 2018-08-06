71d1709af154b76a0b6d8de433ef9b650704189e
sgunnala <sgunnala@DIN73002727.corp.capgemini.com>
2018-08-06 12:20:54 +0530

    Added two numbers

---
+++ Day1GitDemo/src/main/java/com/cap/AddofTwo.java
@@ -0,0 +1,10 @@
+package com.cap;
+
+public class AddofTwo {
+
+	public void sum(int i, int j) {
+		// TODO Auto-generated method stub
+		System.out.println(i+j);
+	}
+	
+}
--- Day1GitDemo/src/main/java/com/cap/MainTestClass.java
+++ Day1GitDemo/src/main/java/com/cap/MainTestClass.java
@@ -4,12 +4,13 @@ public class MainTestClass {
 
 	public static void main(String[] args) {
 		SalesClass class1=new SalesClass();
+		AddofTwo add=new AddofTwo();
 		// TODO Auto-generated method stub
 System.out.println("Hello World");
 class1.show();
+add.sum(2,3);
 greet();
 	}
-
 	public static void greet() {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
